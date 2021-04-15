8833f09a031c02284eb01e203afef452bf8bce77
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-15 17:22:44 +0100

    added custom user, superuser and category model

---
+++ jwt_auth/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    fullName = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, unique=True)
+    profileImage = models.CharField(max_length=300, blank=True)
+    points = models.PositiveIntegerField(blank=True, null=True)
+    
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jwt_auth',
+    'categories'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'habit_tracker',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -123,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'jwt_auth.User'
