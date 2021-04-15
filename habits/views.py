33210ec4c9bfd092060842b05b6cd2f2cf952824
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-15 20:36:52 +0100

    added secure route for habits

--- habits/views.py
+++ habits/views.py
@@ -1,13 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Habit
 from .serializers.common import HabitSerializer
 
 class HabitListView(APIView):
+    permission_classes = (IsAuthenticated,)
 
-	def get(self, _request):
-		habits = Habit.objects.all()	 
-		serialized_habits = HabitSerializer(habits, many=True) 
-		return Response(serialized_habits.data, status=status.HTTP_200_OK)
+    def get(self, _request):
+        habits = Habit.objects.all()	 
+        serialized_habits = HabitSerializer(habits, many=True) 
+        return Response(serialized_habits.data, status=status.HTTP_200_OK)
