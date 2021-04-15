e4cfcaad23bd0b35798c372cb300088a58b79ea8
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-15 17:46:46 +0100

    added get and view request for categories, and error handling

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import NotFound
 
-# Create your views here.
+from .models import Category
+from .serializers.common import CategorySerializer
+
+class CategoryListView(APIView):
+    def get(self, _request):
+        categories = Category.objects.all() 
+        serialized_categories = CategorySerializer(categories, many=True) 
+        return Response(serialized_categories.data, status=status.HTTP_200_OK)
+
+class CategoryDetailView(APIView):
+    def get_category(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound(detail="🚨 Cannot find that category")
+
+
+    def get(self, _request, pk):
+        category = self.get_category(pk=pk)
+        serialized_category = CategorySerializer(category)
+        return Response(serialized_category.data, status=status.HTTP_200_OK)
