66b74aad36d8fc1cefaa0e09a42cde7e4e4ddf0f
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-15 20:58:28 +0100

    post and edit habit secure routes added

--- habits/views.py
+++ habits/views.py
@@ -15,7 +15,15 @@ class HabitListView(APIView):
         serialized_habits = HabitSerializer(habits, many=True) 
         return Response(serialized_habits.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        habit_to_add = HabitSerializer(data=request.data)
+        if habit_to_add.is_valid():
+            habit_to_add.save()
+            return Response(habit_to_add.data, status=status.HTTP_201_CREATED)
+        return Response(habit_to_add.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 class HabitDetailView(APIView):
+    
     def get_habit(self, pk):
         try:
             return Habit.objects.get(pk=pk)
@@ -26,3 +34,11 @@ class HabitDetailView(APIView):
         habit_to_delete = self.get_habit(pk=pk)
         habit_to_delete.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        habit_to_edit = self.get_habit(pk=pk)
+        updated_habit = HabitSerializer(habit_to_edit, data=request.data)
+        if updated_habit.is_valid():
+            updated_habit.save()
+            return Response(updated_habit.data, status=status.HTTP_202_ACCEPTED)
+        return Response(updated_habit.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
