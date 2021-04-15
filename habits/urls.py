1c2819bf59dd7a0312d7e020fecec95394221b24
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-15 20:48:13 +0100

    delete habit route added

--- habits/urls.py
+++ habits/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HabitListView
+from .views import HabitListView, HabitDetailView
 
 urlpatterns = [
-	path('', HabitListView.as_view())
+	path('', HabitListView.as_view()),
+    path('<int:pk>/', HabitDetailView.as_view())
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Habit
@@ -13,3 +14,15 @@ class HabitListView(APIView):
         habits = Habit.objects.all()	 
         serialized_habits = HabitSerializer(habits, many=True) 
         return Response(serialized_habits.data, status=status.HTTP_200_OK)
+
+class HabitDetailView(APIView):
+    def get_habit(self, pk):
+        try:
+            return Habit.objects.get(pk=pk)
+        except Habit.DoesNotExist:
+            raise NotFound(detail="🚨 Cannot find that habit")
+
+    def delete(self, _request, pk):
+        habit_to_delete = self.get_habit(pk=pk)
+        habit_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
