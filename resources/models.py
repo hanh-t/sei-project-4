482fb749a495ad216dcdb061eb769fcb75807c03
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-16 14:04:04 +0100

    get all resources route added

--- resources/models.py
+++ resources/models.py
@@ -4,3 +4,11 @@ class Resource(models.Model):
     resourceType = models.CharField(max_length=50)
     title = models.CharField(max_length=400)
     url = models.CharField(max_length=400)
+    # category = models.ForeignKey(
+    #     "categories.Category",
+    #     related_name="comments",
+    #     on_delete= models.CASCADE
+    # )
+
+    def __str__(self):
+        return f"{self.title} - {self.resourceType}"
--- resources/views.py
+++ resources/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status 
 
-# Create your views here.
+from .models import Resource 
+from .serializers.common import ResourceSerializer
+
+class ResourceListView(APIView):
+    def get(self, _request):
+        resources = Resource.objects.all()
+        serialized_resources = ResourceSerializer(resources, many=True) 
+        return Response(serialized_resources.data, status=status.HTTP_200_OK)
