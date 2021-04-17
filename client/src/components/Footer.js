fd88021e60077be6c259e0eb2c8a5f3a84e5f811
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-17 15:47:18 +0100

    initial components set up

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Footer = () => {
 
   return (
-    <p> Footer</p>
+    <div className="footer">
+      {/* <p><i className="fas fa-leaf"></i></p> */}
+      <p>ICON</p>
+      <p>💟 {'\u00A9'} 2021</p>
+      <a href="#"><p>Contact us here</p></a>
+    </div>
   )
 }
 
--- client/src/components/HabitForm.js
+++ client/src/components/HabitForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const HabitForm = () => {
-
+  
   return (
     <p>Hello</p>
   )
