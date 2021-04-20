e5965ed2f9fa7195c8c342cd54e0a119c43aa572
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-20 23:01:37 +0100

    homepage and footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,9 +5,7 @@ const Footer = () => {
   return (
     <div className="footer">
       {/* <p><i className="fas fa-leaf"></i></p> */}
-      <p>ICON</p>
       <p>🤍 {'\u00A9'} 2021</p>
-      <a href="#"><p>Contact us here</p></a>
     </div>
   )
 }
