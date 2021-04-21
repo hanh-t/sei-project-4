b1ba6a4ae6735ce3729c37453a0890fc94da68d9
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-21 21:09:39 +0100

    embedded vids

--- client/src/components/resources/ResourceShow.js
+++ client/src/components/resources/ResourceShow.js
@@ -27,13 +27,28 @@ const ResourceShow = ({ userData }) => {
       <h1 className="headers">More details</h1>
       <div className="details-container">
         <div className="resource-detail">
-          <h2>{resource.title}</h2>
-          <h2>How to view/where to find: {resource.url}</h2>
+          <h2 className="headers">{resource.title}</h2>
+          {resource.resourceType === 'Video' ?
+
+            ( resource.title === 'Pointing and Calling Japanese Safety Standard' ? 
+
+              <div className="embedded-vid">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/9LmdUz3rOQU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </div>
+              : 
+              <div className="embedded-vid">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/k2h2lvhzMDc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </div>
+            )
+            :
+            <h2>How to view/where to find: {resource.url}</h2>
+          }
           { userIsAuthenticated() ? 
             <h3>Our cost: {resource.price} points</h3>
             :
             <p>Login to see our price</p>
           }
+        
         </div>
         <ResourceWishlist 
           userData = {userData}
