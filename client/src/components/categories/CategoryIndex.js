bde2b8da37e2a47b6ac4f3e6e88dfe4a546124a8
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-22 01:18:48 +0100

    category list layout fixed

--- client/src/components/categories/CategoryIndex.js
+++ client/src/components/categories/CategoryIndex.js
@@ -18,10 +18,12 @@ const Category = () => {
     <>
       <h1 className="headers">CATEGORY LIST</h1>
       <h2 className="headers">Select a category to input details</h2>
-      <div className="animate__animated animate__pulse category-container">
-        { categories.map( category => (
-          <CategoryCard key={category.id} {...category} />
-        ))}
+      <div className="main-cat-container">
+        <div className=" category-container">
+          { categories.map( category => (
+            <CategoryCard key={category.id} {...category} />
+          ))}
+        </div>
       </div>
     </>
   )
