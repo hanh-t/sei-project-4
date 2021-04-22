b2048a7b29099c83c41f353d0926c94f4ba8d6d9
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-22 12:46:29 +0100

    home styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,8 +8,9 @@ const Home = () => {
     <div className="homepage-main">
       <div className="home">
         <h1 className="headers">HABITUP</h1>
+        <h2 className="headers">---UP YOUR HABIT GAME---</h2>
         <h2 className="headers">IF YOU GENUINELY CARE ABOUT THE GOAL, YOU WILL FOCUS ON THE SYSTEM</h2>
-        <h2 className="headers">SIGN UP!</h2>
+        {/* <h2 className="headers">SIGN UP OR</h2> */}
       </div>
     </div>
   )
