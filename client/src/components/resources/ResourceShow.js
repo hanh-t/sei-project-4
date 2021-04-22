0d5860368de51fcd9b7481a9f6c10c3f39dfba23
Hannah Tinacci <hannahtinacci@gmail.com>
2021-04-22 13:04:26 +0100

    vid detail

--- client/src/components/resources/ResourceShow.js
+++ client/src/components/resources/ResourceShow.js
@@ -31,13 +31,17 @@ const ResourceShow = ({ userData }) => {
           {resource.resourceType === 'Video' ?
 
             ( resource.title === 'Pointing and Calling Japanese Safety Standard' ? 
-
+              // <div className="vid-detail">
               <div className="embedded-vid">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/9LmdUz3rOQU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+  
               </div>
               : 
-              <div className="embedded-vid">
+              <div className="embedded-vid-pixar">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/k2h2lvhzMDc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h3>Ed Catmull, cofounder of Pixar, on how they make great movies: <br />
+
+              Initially, the films we put together, they are a mess. It is like everything else in life—the first time you do it, it is a mess. Sometimes labeled ... a failure ... but that is not even the right word to use. It is just like, you get the first one out, you learn from it, and the only failure is if you do not learn from it, if you do not progress.</h3>
               </div>
             )
             :
